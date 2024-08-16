aecd52c9f4caf089acab6626075a72a064020df0
Olaminiyi <olaiyaolaminiyi@gmail.com>
2024-08-16 12:27:32 +0100

    Update solution.py

--- maxProfit/solution.py
+++ maxProfit/solution.py
@@ -2,7 +2,7 @@
 
 # # if you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit
 
-# # Note that you cannot sell a stock before you buy one.
+# # Note that you cannot sell a stock before you buy one
 
 # # Example;
 
