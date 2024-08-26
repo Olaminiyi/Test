5c72f52ec0c10fa4fcc0420da75fa301cba22f0a
olaminiyi <olaiyaolaminiyi@gmail.com>
2024-08-26 16:43:39 +0100

    Regex

--- regex/regex.py
+++ regex/regex.py
@@ -13,19 +13,3 @@ for match in matches:
     print(match)
 #print(text_to_search[1:4])
 
-# -------------------------------------
-# .  - Any character Except New license
-# \d - Digit (0-9)
-# \D - Not a Digit (0-9)
-# \w - word character (a-z, A-Z, 0-9, _)
-# \W - Not a Word Character
-# \s - Whitespace (space, tab, newline)
-# \S - Not Whitespace (space, tab, newline)
-
-# \b - Word Boundaary
-# \B - Not a Word Boundary
-# ^  - Beginning of a String 
-# $  - End of a String 
-
-# [] - Matches Characters in brackets 
-# [^] - Matches Character NOT in brackets
