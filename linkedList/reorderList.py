decbc34f7c6a3608dd1c086c243cd3e09133359a
olaminiyi <olaiyaolaminiyi@gmail.com>
2024-09-07 22:47:38 +0100

    moved reorderList to linkList

---
+++ linkedList/reorderList.py
@@ -0,0 +1,33 @@
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """        
+        if not head.next or not head.next.next:
+            return
+        mid = end = head
+        # split
+        while end.next and end.next.next:
+            end = end.next.next
+            mid = mid.next
+        p2 = mid.next
+        mid.next = None
+        # reverse
+        prev = None
+        while p2 and p2.next:
+            p2next = p2.next
+            p2.next = prev
+            prev = p2
+            p2 = p2next
+        p2.next = prev
+        # merge
+        p1 = head
+        while p2:
+            p1next = p1.next
+            p2next = p2.next
+            p1.next = p2
+            p2.next = p1next
+            p1 = p1next
+            p2 = p2next
